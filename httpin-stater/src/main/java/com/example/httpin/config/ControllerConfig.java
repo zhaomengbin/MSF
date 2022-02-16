7f00e9171cb97bc7e04a4f0e144e6c207102e720
赵孟彬 <zhaomengbin@lattebank.com>
2022-02-16 15:24:05 +0800

    remove

--- httpin-stater/src/main/java/com/example/httpin/config/ControllerConfig.java
+++ httpin-stater/src/main/java/com/example/httpin/config/ControllerConfig.java
@@ -2,8 +2,8 @@ package com.example.httpin.config;
 
 import com.example.httpin.handler.GlobalExceptionHandler;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * 〈〉
@@ -13,8 +13,12 @@ import org.springframework.context.annotation.Import;
  * @since 1.0.0
  */
 @Configuration
-@ConditionalOnWebApplication(type=ConditionalOnWebApplication.Type.SERVLET)
-@Import({GlobalExceptionHandler.class})
 public class ControllerConfig {
+
+    @Bean
+    @ConditionalOnWebApplication(type=ConditionalOnWebApplication.Type.SERVLET)
+    public GlobalExceptionHandler globalExceptionHandler(){
+        return new GlobalExceptionHandler();
+    }
 
 }
